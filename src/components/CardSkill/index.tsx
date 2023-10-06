7dd2ed0227b232021c4040166d0fc3ef84e1165d
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-10-06 17:45:55 -0300

    fix title Swift

--- src/components/CardSkill/index.tsx
+++ src/components/CardSkill/index.tsx
@@ -17,7 +17,7 @@ import TitleSkills from '../TitleSkills';
 const skillsData = [
   { src: reactjs, alt: 'React Native', description: 'React Native' },
   { src: nodejs, alt: 'NodeJS', description: 'NodeJS' },
-  { src: siwft, alt: 'Siwft', description: 'Siwft' },
+  { src: siwft, alt: 'Swift', description: 'Swift' },
   { src: postgres, alt: 'PostgreSQL', description: 'PostgreSQL' },
   { src: reactjs, alt: 'ReactJS', description: 'ReactJS' },
   { src: figma, alt: 'Figma', description: 'Figma' },
