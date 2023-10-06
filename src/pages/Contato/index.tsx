1c9fedb3139c937a7f14f2a6a50ddd1f85226e1a
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-10-06 17:56:56 -0300

    fix mobile version

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -5,7 +5,7 @@ function Contato() {
     <>
       <Description style={{ paddingTop: 600 }} id='contatos'>
 
-        <Title> Tersina, PI - Brasil </Title>
+        <Title> Teresina, PI - Brasil </Title>
         <Title> welliton.araujo@uol.com.br </Title>
         <Title> 91 98571-5267 </Title>
         <Title> Contato </Title>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <Description>
           <TextDescription children='Olá, sou o' style={{ margin: 0 }} />
           <Welliton>Welliton Araujo</Welliton>
-          <Title>Desenvolvedor Mobile  <br /> <StyledAmpersand>&</StyledAmpersand> Front-end </Title>
+          <Title>Desenvolvedor Mobile <StyledAmpersand>&</StyledAmpersand> Front-end </Title>
 
           <TextDescription><Line>Com anos de experiência</Line> do mercado de desenvolvimento de software e apaixonado por tecnologia, busco aperfeiçoamento profissional diariamente.</TextDescription>
           <TextDescription><Line>Sólida experiência</Line> em todo ciclo de desenvolvimento mobile, da prototipação, arquitetura, desenvolvimento, testes e deploy.</TextDescription>
