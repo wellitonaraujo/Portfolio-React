ab13d92b48ecd0114d47ef0cccf02ac68e98e205
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-10-06 18:16:31 -0300

    fix layout

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -4,11 +4,10 @@ function Contato() {
   return (
     <>
       <Description style={{ paddingTop: 600 }} id='contatos'>
-
         <Title> Teresina, PI - Brasil </Title>
         <Title> welliton.araujo@uol.com.br </Title>
         <Title> 91 98571-5267 </Title>
-        <Title> Contato </Title>
+
       </Description>
     </>
   )
