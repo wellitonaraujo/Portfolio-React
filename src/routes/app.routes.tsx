a19763ad051856b46fe8cf6cb2d9fb8c5b8c43e7
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-10-06 17:16:29 -0300

    remove files

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
-import Skills from '../pages/Skills'
-import Projetos from '../pages/Projetos'
 import Contato from '../pages/Contato'
 
 const AppRoutes = () => {
@@ -9,8 +7,6 @@ const AppRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/sobre' element={<Projetos />} />
         <Route path='/contato' element={<Contato />} />
       </Routes>
     </BrowserRouter>
