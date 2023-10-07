e2d45af3f9b2c01f624b31869273951d50348bfd
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-10-06 21:14:40 -0300

    add links email end wpp

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -1,12 +1,20 @@
-import { Description, Title } from './styles'
+import { Description, Title, EmailLink, WhatsappLink } from './styles'
+
+const EmailAddress = 'welliton.araujo@uol.com.br';
+const PhoneNumber = '5591985715267';
 
 function Contato() {
   return (
     <>
-      <Description style={{ paddingTop: 600 }} id='contatos'>
-        <Title> Teresina, PI - Brasil </Title>
-        <Title> welliton.araujo@uol.com.br </Title>
-        <Title> (91) 98571-5267 </Title>
+      <Description style={{ paddingTop: 500 }} id='contatos'>
+        <Title style={{ marginBottom: 40 }}> Teresina, PI - Brasil </Title>
+
+        <EmailLink style={{ marginTop: 40 }} href={`mailto:${EmailAddress}`}>{EmailAddress}</EmailLink>
+
+        <EmailLink href={`https://wa.me/${PhoneNumber}`} target="_blank">
+          <Title style={{ marginTop: 40 }} >(91) 98571-5267</Title>
+        </EmailLink>
+
       </Description>
     </>
   )
--- src/pages/Contato/styles.ts
+++ src/pages/Contato/styles.ts
@@ -15,6 +15,22 @@ export const Description = styled.div`
   }
 `;
 
+export const EmailLink = styled.a`
+   font-size: 3.5rem;
+  background: linear-gradient(to bottom,  ${colors.primary.s400}, ${colors.primary.s300});
+  -webkit-background-clip: text; 
+  background-clip: text; 
+  color: transparent;
+  margin: 0;
+  font-weight: 800;
+
+  ${mobileMediaQuery} {
+    flex-direction: column; /* Altere para uma coluna em dispositivos móveis */
+    font-weight: 500;
+    font-size: 1.2rem;
+    text-align: left;
+  }
+`;
 
 export const Title = styled.p`
   font-size: 3.5rem;
@@ -28,7 +44,7 @@ export const Title = styled.p`
   ${mobileMediaQuery} {
     flex-direction: column; /* Altere para uma coluna em dispositivos móveis */
     font-weight: 500;
-    font-size: 1rem;
+    font-size: 1.2rem;
     text-align: left;
   }
 `;
